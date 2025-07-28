80e3db11e94bcedef4e1dbedb9e1553b803f7e11
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-07-28 18:53:47 +0100

    Update fastAPI with CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,25 @@
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .database import Base, engine, SessionLocal
+from fastapi import FastAPI
 from .models import User
-from .utils.security import hash_password
 from .routes import auth
 
 app = FastAPI()
+
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=["http://localhost:5173"],  # React dev server
+   allow_credentials=True,
+   allow_methods=["*"],
+   allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
 def init_admin():
    db = SessionLocal()
    if not db.query(User).filter(User.username == "admin").first():
-      admin = User(username="admin", password=hash_password("admin"))
+      admin = User(username="admin", password="$2b$12$5xrx/YgB4y593kiBIZ.FNecYSmnqu9mwN.s/0zcvPJZUGCyWRKsmK")
       db.add(admin)
       db.commit()
    db.close()
