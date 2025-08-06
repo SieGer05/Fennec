2537deaf7e188a8f9d80de3291cd3f60614634b4
SieGer <aminedjili29@gmail.com>
2025-08-06 12:09:37 +0100

    more readable

--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -1,5 +1,5 @@
 export async function login(username, password) {
-        const response = await fetch("http://127.0.0.1:8000/auth/login", {
+    const response = await fetch("http://127.0.0.1:8000/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
